124b95066aa99fb8700e2422d49bea8bd54958ca
Gerrrt <garrettallen2@gmail.com>
2026-06-01 07:08:55 -0700

    Squashed 'core/' changes from 7f05bf9..382cef1
    382cef1 Update nvim settings
    
    git-subtree-dir: core
    git-subtree-split: 382cef175f59f3e0020a87277d5236dbaeacee1b

--- nvim/lua/gerrrt/plugins/nvim-tree.lua
+++ nvim/lua/gerrrt/plugins/nvim-tree.lua
@@ -30,7 +30,7 @@ return {
 	config = function()
 		require("nvim-tree").setup({
 			filters = { dotfiles = false },
-			view = { adaptive_size = true },
+			view = { adaptive_size = false },
 		})
 	end,
 }
